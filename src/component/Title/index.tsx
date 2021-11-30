a29c8eea836df3ada6613660ae3ce54b8f24b3eb
WRXM4STER <montaev.a@mail.ru>
2021-11-30 22:54:19 +0300

    Добавлен шрифт IBM Plex Sans

--- src/component/Title/index.tsx
+++ src/component/Title/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import style from './style.module.scss'
 
 const Title:React.FC = props => {
-    return (<div className={style.title}>{props.children}</div>)
+    return (<h1 className={style.title}>{props.children}</h1>)
 }
 
 export default Title
